18fddd67e21472832c9ff52c90d2f27463c6e0ad
xXTHUND3RXx <fd192168@gmail.com>
2025-07-16 15:34:41 -0300

    :lipstick: feat: registration page styling

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,13 +1,21 @@
-'use client';
+"use client";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { registerValidation, RegisterValidation } from "@/utils/registerValidation";
+import {
+  registerValidation,
+  RegisterValidation,
+} from "@/utils/registerValidation";
 import { registerUser } from "@/services/auth";
 
 export default function RegisterPage() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<RegisterValidation>({
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<RegisterValidation>({
     resolver: zodResolver(registerValidation),
   });
 
@@ -16,7 +24,13 @@ export default function RegisterPage() {
 
   const onSubmit = async (data: RegisterValidation) => {
     try {
-      await registerUser(data.nome, data.email, data.senha, data.tipo_usuario, data.senha_admin);
+      await registerUser(
+        data.nome,
+        data.email,
+        data.senha,
+        data.tipo_usuario,
+        data.senha_admin
+      );
       router.push("/login");
     } catch (error) {
       console.error("Erro no cadastro:", error);
@@ -24,19 +38,55 @@ export default function RegisterPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("nome")} placeholder="Nome" />
-      <input {...register("email")} placeholder="Email" />
-      <input type="password" {...register("senha")} placeholder="Senha" />
-      <select {...register("tipo_usuario")}>
-        <option value="usuario">Usuário</option>
-        <option value="admin">Admin</option>
-      </select>
-      {tipo_usuario === "admin" && (
-        <input type="password" {...register("senha_admin")} placeholder="Senha de admin" />
-      )}
-      <button type="submit">Cadastrar</button>
-      <pre>{JSON.stringify(errors, null, 2)}</pre>
-    </form>
+    <section className="bg-[#ffffff] text-gray-700 w-full h-100 flex justify-center items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center gap-2"
+      >
+        <input
+          {...register("nome")}
+          placeholder="Nome"
+          className="input-base"
+        />
+        {errors.nome && <p>{errors.nome.message?.toString()}</p>}
+        <input
+          {...register("email")}
+          placeholder="Email"
+          className="input-base"
+        />
+        {errors.email && <p>{errors.email.message?.toString()}</p>}
+        <input
+          type="password"
+          {...register("senha")}
+          placeholder="Senha"
+          className="input-base"
+        />
+        {errors.senha && <p>{errors.senha.message?.toString()}</p>}
+        <select
+          {...register("tipo_usuario")}
+          className="rounded-lg border-(--main-color) border-solid border-2"
+        >
+          <option value="usuario">Usuário</option>
+          <option value="admin">Admin</option>
+        </select>
+        {tipo_usuario === "admin" && (
+          <input
+            type="password"
+            {...register("senha_admin")}
+            placeholder="Senha de administrador"
+            className="input-base"
+          />
+        )}
+        <button
+          type="submit"
+          className="flex items-center gap-2 bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition duration-200"
+        >
+          Cadastrar
+        </button>
+        {/* {Object.keys(errors).length > 0 && (
+          <pre className="text-red-500 bg-red-100 p-2 rounded">{JSON.stringify(errors, null, 2)}</pre>
+        )} */}
+      </form>
+    </section>
   );
 }
